1788e8538ead5fbe3efa6310e68abe5512a6783d
developertarik <tarikozgur0607@gmail.com>
2023-08-26 11:02:42 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -21,39 +21,49 @@ const titleInput = document.getElementById("title")
  const checkbox = document.createElement("input");
  const a = document.createElement("a")
  checkbox.setAttribute("type","checkbox");
+ checkbox.classList.add("check");
+ const dat =document.createElement("input");
+ dat.setAttribute("type","date");
  
 
 
 function addTask() {
       
-   
+    button.addEventListener("click",function(){
+        newTask = new Todo(title);
+       const box = document.createElement("div")
+        box.classList.add("box")
+         analog.appendChild(div2)
+        console.log(title.value );
+        box.appendChild(checkbox)
+        box.appendChild(dat)
+    
+    
+             box.innerHTML +=   "<p>" +title.value + "</p>"  
+             analog.appendChild(div2)
+             container.appendChild(newheader)
+          container.appendChild(box)
+         
+        titleInput.value = "";
+    
+    });
+    titleInput.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            button.click();
+        }
+    });
+     function remove( ) {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      }
+     
+      checkbox.addEventListener("click",()=>{
+    
+      });
   
 }
-button.addEventListener("click",function(){
-    newTask = new Todo(title);
-   const box = document.createElement("div")
-    box.classList.add("box")
-     analog.appendChild(div2)
-    console.log(title.value     );
-         box.innerHTML += title.value
-        box.appendChild(checkbox)
-         analog.appendChild(div2)
-                    container.appendChild(newheader)
-      container.appendChild(box)
-      box.innerHTML += a.value
-    
-    titleInput.value = "";
+addTask();
 
-});
-titleInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        button.click();
-    }
-});
- 
-
-
-
-
+// yap
+// boxun içine tarih ekle ve boxun width ini büyüy ve checkbox basınca kaldır
   
