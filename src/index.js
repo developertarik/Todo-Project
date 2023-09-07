8d25e25d40535088fca5892316eaf4dcfc34a513
developertarik <tarikozgur0607@gmail.com>
2023-09-08 02:13:05 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
- function Todo(title, description, dueDate, priority) {
+function Todo(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
 
 }
- 
+
 //doms
 const div2 = document.createElement("div");
 const container = document.getElementById("container");
@@ -14,52 +14,49 @@ const header = document.createElement("h1");
 const form = document.getElementById("form");
 const button = document.getElementById("btn");
 const titleInput = document.getElementById("title")
- const a = document.createElement("a");
-  const projects = document.getElementById("projectsDiv")
-  const projectsBtn = document.getElementById("projectsBtn")
- const inboxBtn = document.getElementById("inboxBtn")
- const dat = document.createElement("input");
+const a = document.createElement("a");
+const projects = document.getElementById("projectsDiv")
+const projectsBtn = document.getElementById("projectsBtn")
+const inboxBtn = document.getElementById("inboxBtn")
+const dat = document.createElement("input");
 dat.setAttribute("type", "date");
 function addTask() {
-  
-  button.addEventListener("click", function () {
-  
-        newTask = new Todo(title );
-        const box = document.createElement("li")
 
-         box.classList.add("box")
+    button.addEventListener("click", function () {
+        newTask = new Todo(title);
+
+        const box = document.createElement("li")
+        box.classList.add("box")
+
         analog.appendChild(div2)
         console.log(title.value);
 
         box.appendChild(dat)
+        box.innerHTML += "<p class=`myTitle`>" + title.value + "</p>"
 
-
-
-        box.innerHTML += "<p class=`myTitle`>" + title.value + "</p>"  
-        
-          const checkbox = document.createElement("button");
+        const checkbox = document.createElement("button");
 
         checkbox.innerText = "Complete task!"
-        
-            checkbox.addEventListener("click",()=>{
-                box.remove();
-                checkbox.remove();
-        })  
-      
-        
+
+        checkbox.addEventListener("click", () => {
+            box.remove();
+            checkbox.remove();
+        })
+
+
         box.appendChild(checkbox)
- 
- 
+
+
         analog.appendChild(div2)
-        localStorage.setItem("contentDiv",container.innerHTML);
+        localStorage.setItem("contentDiv", container.innerHTML);
 
         container.appendChild(box)
 
         titleInput.value = "";
 
-    
+
     });
-  
+
     titleInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -69,22 +66,24 @@ function addTask() {
 
 
 }
- 
-addTask();
-  
 
-inboxBtn.addEventListener("click",()=>{
+addTask();
+
+
+inboxBtn.addEventListener("click", () => {
     console.log("this page, come again inbox")
 })
- 
+
 function addProject() {
-    projectsBtn.addEventListener("click",()=>{ 
+    const projects2 = document.createElement("li");
 
-    const projects2= document.createElement("li")
-    const projectsInput = document.getElementById("projectName")
-    projects2.innerHTML += "<p class=`myTitle`>" + projectsInput.value + "</p>"  
+    projectsBtn.addEventListener("click", () => {
 
-         projects.appendChild(projectsInput)
+        projects2.innerHTML = "<a href='#'>my Projects</a>"
+        projects.appendChild(projects2)
+    })
+    projects2.addEventListener("click",()=>{
+         container.innerText= projects2.innerText
     })
 }
 
