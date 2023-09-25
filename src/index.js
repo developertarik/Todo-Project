3e8067bb0ca7633e1360861000d8f3187fcfc6c9
developertarik <tarikozgur0607@gmail.com>
2023-09-25 18:49:33 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const todos = [];
 function Todo(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
@@ -18,36 +17,48 @@ const titleInput = document.getElementById("title")
 const a = document.createElement("a");
 const projects = document.getElementById("projectsDiv")
 const projectsBtn = document.getElementById("projectsBtn")
-const projectsInput = document.getElementById("projectsInput")
 const inboxBtn = document.getElementById("inboxBtn")
 const dat = document.createElement("input");
 container.innerText = "Inbox"
-const projectForm = document.getElementById("project-form")
+
 dat.setAttribute("type", "date");
 function addTask() {
+
     button.addEventListener("click", function () {
-        task = new Todo(title);
-        todos.push(task);
-        console.log(todos)
+        newTask = new Todo(title);
+
         const box = document.createElement("li")
         box.classList.add("box")
+
         analog.appendChild(div2)
         console.log(title.value);
+
         box.appendChild(dat)
         box.innerHTML += "<p class=`myTitle`>" + title.value + "</p>"
+
         const checkbox = document.createElement("button");
+
         checkbox.innerText = "Complete task!"
+
         checkbox.addEventListener("click", () => {
             box.remove();
             checkbox.remove();
         })
+
+
         box.appendChild(checkbox)
-        localStorage.setItem("Inbox", container.innerHTML)
+
+        localStorage.setItem("Inbox",container.innerHTML)
         analog.appendChild(div2)
         localStorage.setItem("contentDiv", container.innerHTML);
+
         container.appendChild(box)
+
         titleInput.value = "";
+
+
     });
+
     titleInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -63,7 +74,7 @@ addTask();
 
 inboxBtn.addEventListener("click", () => {
     container.innerText = "Inbox"
-    container.innerHTML = localStorage.getItem("Inbox", container.innerHTML);
+    container.innerHTML =  localStorage.getItem("Inbox",container.innerHTML);
 
 })
 
@@ -71,24 +82,21 @@ function addProject() {
     const projects2 = document.createElement("li");
 
     projectsBtn.addEventListener("click", () => {
-        projectForm.style.visibility = "visible";
-
+        const projectName = prompt("What is the project name ?")
+        projects2.innerHTML += "<a href='#'>" + projectName + "</a>" + "<br>"
+        projects.appendChild(projects2)
+        localStorage.setItem('project', projects2.innerHTML);
         // Check for saved wishlist items
-
+ 
 
     })
     
 
-    projectForm.addEventListener("submit", (e) => {
-
-    })
-
+ projects2.addEventListener("click",function( e){
+    console.log("you clicked this element:",e.target)
+     
+  })
+ 
 }
-
-projectsInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-    }
-});
 
 addProject()
