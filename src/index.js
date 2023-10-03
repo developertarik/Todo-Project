84eae22212be96badb3cd8a5fd294c966b120f7c
developertarik <tarikozgur0607@gmail.com>
2023-10-03 23:13:17 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const todos = [];
+const todos = ["task1"];
+const projects = ["task2"];    
 function Todo(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
@@ -7,83 +8,25 @@ function Todo(title, description, dueDate, priority) {
 
 }
 
-//doms
-const div2 = document.createElement("div");
-const container = document.getElementById("container");
-const analog = document.getElementById("todo");
-const header = document.createElement("h1");
-const form = document.getElementById("form");
-const button = document.getElementById("btn");
-const titleInput = document.getElementById("title")
-const a = document.createElement("a");
-const projects = document.getElementById("projectsDiv")
-const projectsBtn = document.getElementById("projectsBtn")
-const projectsInput = document.getElementById("projectsInput")
-const projectList = document.getElementById("projects-List")
- const dat = document.createElement("input");
-container.innerText = "Inbox"
-const projectForm = document.getElementById("project-form")
-dat.setAttribute("type", "date");
-function addTask() {
-    button.addEventListener("click", function () {
-        task = new Todo(title);
-        todos.push(task);
-        console.log(todos)
-        const box = document.createElement("li")
-        box.classList.add("box")
-        analog.appendChild(div2)
-        console.log(title.value);
-        box.appendChild(dat)
-        box.innerHTML += "<p class=`myTitle`>" + title.value + "</p>"
-        const checkbox = document.createElement("button");
-        checkbox.innerText = "Complete task!"
-        checkbox.addEventListener("click", () => {
-            box.remove();
-            checkbox.remove();
-        })
-        box.appendChild(checkbox)
-        localStorage.setItem("Inbox", container.innerHTML)
-        analog.appendChild(div2)
-        localStorage.setItem("contentDiv", container.innerHTML);
-        container.appendChild(box)
-        titleInput.value = "";
+ const projectList = document.getElementById("projectList")
+ const newProjects = document.createElement("li")
+function dom() {
+     
+ 
+    projects.forEach(element => {
+       newProjects.innerHTML += element  
+       projectList.appendChild(newProjects)     
     });
-    titleInput.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            button.click();
-        }
-    });
-
-
 }
+function addTask(){
+     todos.forEach(element=>{
+        container.innerHTML +=element 
 
+    })
+}
+dom();
 addTask();
 
-
- 
-
-function Project(){
-const projects2 = document.getElementById("projects-List");
-localStorage.setItem('project', projects2.innerHTML);
-
-      projectsBtn.addEventListener("click", () => {
-        const projectName = document.getElementById("projectsName");
-         projects2.innerHTML += "<li href='#'>" + projectName.value + "</li>" + "<br>"
-         projects.appendChild(projects2)
-        localStorage.getItem('project', projects2.innerHTML);
-       
-
-     })
-     projects2.addEventListener("click", () => {
-         container.innerText = projects2.innerText
-    })
-     let saved = localStorage.getItem('project');
-      if (saved) {
-       projects2.innerHTML = saved;
- }
-}
-
-Project()
- 
- 
+// let todo = new Todo("My title", "My todo description","08/2023"), true)
+// const array = []
+// array.push(todo)
