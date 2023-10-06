8565fd129f7ca858306f712efa3a95c0fadbbd38
developertarik <tarikozgur0607@gmail.com>
2023-10-06 20:28:11 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -31,28 +31,36 @@
     }
     function addTask(){
         btn.addEventListener("click", function (e) {
-            
-            todo.title = e.target.value
+            const input = document.getElementById("title")
+            const a = document.createElement("a")
+            a.innerText = input.value
+            todo.title = input.value
+            const date = document.createElement("input");
+            date.setAttribute("type","date")
+            todo.dueDate = date
             todos.push(todo)
             console.log(todos)
             projects.push(todos)
 
             const box = document.createElement("li")
             box.classList.add("box")
+            box.appendChild(date)
              const checkbox = document.createElement("button");
             checkbox.innerText = "Complete task!";
+            box.appendChild(a)
             box.appendChild(checkbox)
             localStorage.setItem("Inbox",container.innerHTML)
             container.appendChild(box)    
            
         });
-        todos.forEach(element=>{
-            todos.push(todo)
-            console.log(todos)
- 
-        })
+       
        
     }
+    todos.forEach(element=>{
+        todos.push(todo)
+        console.log(todo)
+
+    })
     dom();
     addTask();
 
