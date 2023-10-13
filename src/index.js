262d86ee97d3d6e7fc727d06ffaaf61569a9c042
developertarik <tarikozgur0607@gmail.com>
2023-10-14 00:31:07 +0300

    add new code s

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-    const todos = ["test"];
-    const projects = ["project1"];    
-    const btn = document.getElementById("btn")
-    const projectBtn = document.getElementById("pBtn")
-    const projectDiv = document.getElementById("projectsDiv")
+    const todos = ["todo"];
+    const projects = ["project"];    
+    const button = document.getElementById("btn")
     const container = document.getElementById("container")
+    const box = document.createElement("div")
+    const input = document.getElementById("title")
+
+    box.classList.add("box")
     function Todo(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
@@ -11,61 +13,49 @@
         this.priority = priority;
 
     }
-    let todo = new Todo("a");
-    const projectList = document.getElementById("projectList")
-    const newProjects = document.createElement("li")
-    function dom() {
-        projectBtn.addEventListener("click",function(e){
-                projects.push(e)
-                console.log(projects)
-                const projectBox = document.createElement("li")
-                projectBox.innerHTML = e.target
-                
-        })
-        
-
-        projects.forEach(element => {
-      projects.push(element)
-      console.log(projects)
-        });
+    function Project(projectTitle,todo){
+        this.projectTitle = projectTitle;
+        this.todo = todo;
     }
-    function addTask(){
-        btn.addEventListener("click", function (e) {
-            const input = document.getElementById("title")
+ 
+
+    let todo = new Todo("i love you theodin","thanks","29.05.2023",2);
+/*
+  const input = document.getElementById("title")
             const a = document.createElement("a")
             a.innerText = input.value
             todo.title = input.value
             const date = document.createElement("input");
             date.setAttribute("type","date")
             todo.dueDate = date
-            todos.push(todo)
-            console.log(todos)
-            projects.push(todos)
-
-            const box = document.createElement("li")
+             const box = document.createElement("li")
             box.classList.add("box")
-            box.appendChild(date)
-             const checkbox = document.createElement("button");
-            checkbox.innerText = "Complete task!";
             box.appendChild(a)
             box.appendChild(checkbox)
             localStorage.setItem("Inbox",container.innerHTML)
             container.appendChild(box)    
            
-        });
-       
-       
-    }
-    todos.forEach(element=>{
-        todos.push(todo)
-        console.log(todo)
+*/
 
-    })
-    dom();
-    addTask();
+function addTodo(){
+    //doms
+    const date = document.createElement("input");
+    date.setAttribute("type","date")
+    const checkbox = document.createElement("button");
+    checkbox.innerText = "Complete task!";
 
+    todo.dueDate = date
+
+    todos.push(todo) 
+    todo.title = input.value;
+    box.appendChild(checkbox)
+    box.appendChild(date)
+
+    console.log(todos)
+    container.appendChild(box)
     
+}
 
-    // let todo = new Todo("My title", "My todo description","08/2023"), true)
-    // const array = []
-    // array.push(todo)
+
+button.addEventListener("click",addTodo);
+
